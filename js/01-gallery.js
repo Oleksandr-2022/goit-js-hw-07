067ee7a58bf95437181dec3c29bc473311cd8f98
Oleksandr-2022 <zibyuk.2017@gmail.com>
2022-10-27 16:30:31 +0300

    HW №7 (1)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,67 +1,4 @@
-//     import { galleryItems } from './gallery-items.js';
-// // Change code below this line
-
-// //1 Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// const galleryList = document.querySelector(".gallery");
-// const galleryImage = document.querySelectorAll(".gallery__image");
-// const galleryMarkup = createGallerySmallPictureCard(galleryItems);
-// let instance;
-
-// galleryList.innerHTML = galleryMarkup;
-
-
-// //2 Реалізація делегування на div.gallery і отримання url великого зображення.
-// galleryList.addEventListener("click", onGalleryImageClick);
-
-// // Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>,
-// // і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-
-// function createGallerySmallPictureCard(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `<div class="gallery__item">
-//         <a class="gallery__link" href="${original}">
-//         <img
-//         class="gallery__image"
-//         src="${preview}"
-//         data-source="${original}"
-//         alt="${description}"
-//         />
-//         </a>
-//         </div>`;
-//     })
-//     .join("");
-// }
-
-// function onGalleryImageClick(e) {
-//     //     e.preventDefault();
-//     //     galleryImage.src = e.target.dataset.source;
-//     //     instance = basicLightbox.create(`<img src="${galleryImage.src}">`);
-//     onModalImageOpen(instance);
-// }
-
-// function onModalImageOpen(e) {
-//     // window.addEventListener("keydown", onEscKeyPress);
-//     document.addEventListener("keydown", onEscKeyPress);
-//     e.show();
-// }
-
-// function onModalImageClose(e) {
-//     // window.removeEventListener("keydown", onEscKeyPress);
-//     document.removeEventListener("keydown", onEscKeyPress);
-//     e.close();
-// }
-
-// function onEscKeyPress(e) {
-//     if (e.code === "Escape") {
-//       onModalImageClose(instance);
-//     }
-// }
-
-// // console.log(galleryItems);
-
-
-  import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const listGalleryRef = document.querySelector(".gallery");
@@ -105,12 +42,12 @@ function onListGalleryClick(event) {
 }
 
 function getModalImageOpen(event) {
-  window.addEventListener("keydown", onPressEscape);
+  document.addEventListener("keydown", onPressEscape);
   event.show();
 }
 
 function getModalImageClose(event) {
-  window.removeEventListener("keydown", onPressEscape);
+  document.removeEventListener("keydown", onPressEscape);
   event.close();
 }
 

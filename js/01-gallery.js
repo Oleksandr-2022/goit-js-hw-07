6b0cba4513141b021ce9285cc5f1736b16ba947b
Oleksandr-2022 <zibyuk.2017@gmail.com>
2022-10-28 23:39:15 +0300

    зміни 28.10

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -36,23 +36,48 @@ function onListGalleryClick(event) {
   const originalSrc = event.target.dataset.source;
 
   instance = basicLightbox.create(`<img src="${originalSrc}">`, {
-    closable: true,
-  });
-  getModalImageOpen(instance);
+    onShow: () => {
+      console.log("Додав ESC");
+      document.addEventListener("keydown", onPressEscape);
+    },
+    onClose: () => {
+      console.log("Прибрав ESC");
+      document.removeEventListener("keydown", onPressEscape);
+    },
+  }
+);
+instance.show();
 }
-
-function getModalImageOpen(event) {
-  document.addEventListener("keydown", onPressEscape);
-  event.show();
-}
-
-function getModalImageClose(event) {
-  document.removeEventListener("keydown", onPressEscape);
-  event.close();
-}
-
 function onPressEscape(event) {
-  if (event.code === "Escape") {
-    getModalImageClose(instance);
+  if (event.key === "Escape") {
+    instance.close(() => {
+      console.log("Закрив, як нажав ESC");
+    });
   }
 }
+
+// function getModalImageOpen(event) {
+//   document.addEventListener("keydown", onPressEscape);
+//   event.show();
+// }
+
+// function getModalImageClose(event) {
+//   document.removeEventListener("keydown", onPressEscape);
+//   event.close();
+// }
+
+// function onPressEscape(event) {
+//   if (event.code === "Escape") {
+//     getModalImageClose(instance);
+//   }
+// }
+
+
+// Доброго дня, Олександр) 
+// У вас слухач знімається тільки при натисканні клавіші esc. Перечитайте  мої коментарі та подивіться уважно, як зняти слухач за допомогою методу onClose бібліотеки basicLightbox.
+
+
+// Доброго вечора, Олександр)
+// По першому завданню подивіться тут https://github.com/electerious/basicLightbox як можна реалізувати зняття слухача повністю при закритті модалки. Враховуйте, що слухач потрібно знімати і при закритті модалки по кліку.
+// Подивіться, будь ласка, методи onShow та onClose бібліотеки basicLightbox.
+// Також зверніть увагу, що слухач клавіатури додається на document, а не на window. 
